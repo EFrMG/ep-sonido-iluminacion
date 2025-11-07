7a67d1a483442681c242a30889ec581ad2c08b98
Ilovelovethis <204592755+Ilovelovethis@users.noreply.github.com>
2025-11-07 05:52:55 -0300

    feature: developer contact modal form
    style: small gradient on the gallery cards
    change: replaced a textual content paragraph at calendar page

--- src/scripts/contacto.js
+++ src/scripts/contacto.js
@@ -2,6 +2,7 @@ import "../styles/styles.css";
 import "./globals.js";
 import "../styles/contacto.css";
 
+// EPSI form
 const mainForm = document.getElementById("contact-form");
 const emailInput = document.getElementById("email");
 const andOrPara = document.querySelector(".and-or-para");
@@ -87,7 +88,9 @@ mainForm.addEventListener("submit", async (e) => {
         }, 1000);
       }, 10000);
 
-      mainForm.reset();
+      setTimeout(() => {
+        mainForm.reset();
+      }, 2500);
     } else {
       const data = await response.json();
 
@@ -154,5 +157,152 @@ formModalButton.addEventListener("click", () => {
   setTimeout(() => {
     formModal.close();
     formModal.style.opacity = "unset";
+  }, 1000);
+});
+
+// Dev form
+const openDevModal = document.getElementById("open-dev-modal");
+const devContactModal = document.getElementById("dev-contact-dialog");
+const closeDevModalButton = document.getElementById("close-dev-modal-button");
+const devForm = document.getElementById("dev-contact-form");
+const devSubmitButton = document.getElementById("dev-submit-button");
+const devBarLoader = document.querySelector(".loader.dev");
+const devFormModal = document.getElementById("dev-form-modal");
+const devFormModalSetText = devFormModal.querySelector("p");
+const devInitialText = devFormModalSetText.innerText;
+const devFormModalButton = document.getElementById("dev-form-modal-button");
+
+openDevModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  devContactModal.showModal();
+});
+
+closeDevModalButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  setTimeout(() => {
+    devContactModal.close();
+    devContactModal.style.removeProperty("--modal-opacity");
+  }, 1100);
+  devContactModal.style.setProperty("--modal-opacity", "0");
+});
+
+devContactModal.addEventListener("cancel", (e) => {
+  e.preventDefault();
+  setTimeout(() => {
+    devContactModal.close();
+    devContactModal.style.removeProperty("--modal-opacity");
+  }, 1100);
+  devContactModal.style.setProperty("--modal-opacity", "0");
+});
+
+// AJAX operations with formspree
+devForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  devBarLoader.classList.add("shown");
+  devSubmitButton.disabled = true;
+
+  const formData = new FormData(devForm);
+
+  try {
+    const response = await fetch(devForm.action, {
+      method: devForm.method,
+      body: formData,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (response.ok) {
+      setTimeout(() => {
+        devBarLoader.style.opacity = "0";
+        setTimeout(() => {
+          devBarLoader.classList.remove("shown");
+          devBarLoader.style.opacity = "unset";
+        }, 171);
+        devFormModal.show();
+        devSubmitButton.disabled = false;
+      }, 2800);
+
+      setTimeout(() => {
+        devFormModal.style.opacity = "0";
+        setTimeout(() => {
+          devFormModal.style.opacity = "unset";
+          devFormModal.close();
+        }, 1000);
+      }, 10000);
+
+      setTimeout(() => {
+        setTimeout(() => {
+          devContactModal.close();
+          devContactModal.style.removeProperty("--modal-opacity");
+        }, 1100);
+        devContactModal.style.setProperty("--modal-opacity", "0");
+        devForm.reset();
+      }, 2500);
+    } else {
+      const data = await response.json();
+
+      devFormModalSetText.innerText = data.errors
+        ? data.errors.map((error) => error.message).join(", ")
+        : "¡Error al enviar!";
+      devFormModal.show();
+
+      devBarLoader.classList.add("failed");
+      devFormModal.classList.add("failed");
+
+      setTimeout(() => {
+        devBarLoader.style.opacity = "0";
+        setTimeout(() => {
+          devBarLoader.classList.remove("shown");
+          devBarLoader.style.opacity = "unset";
+          devBarLoader.classList.remove("failed");
+        }, 1000);
+
+        devFormModal.style.opacity = "0";
+        setTimeout(() => {
+          devFormModal.close();
+          devFormModal.style.opacity = "unset";
+          devFormModal.classList.remove("failed");
+          devFormModalSetText.innerText = devInitialText;
+        }, 1000);
+
+        devSubmitButton.disabled = false;
+      }, 10000);
+    }
+  } catch (error) {
+    devFormModalSetText.innerText = "¡Ocurrió un error!";
+    devFormModal.show();
+
+    devBarLoader.classList.add("failed");
+    devFormModal.classList.add("failed");
+    setTimeout(() => {
+      devBarLoader.style.opacity = "0";
+
+      setTimeout(() => {
+        devBarLoader.classList.remove("shown");
+        devBarLoader.style.opacity = "unset";
+        devBarLoader.classList.remove("failed");
+      }, 1000);
+
+      devFormModal.style.opacity = "0";
+
+      setTimeout(() => {
+        devFormModal.close();
+        devFormModal.style.opacity = "unset";
+        devFormModal.classList.remove("failed");
+        devFormModalSetText.innerText = devInitialText;
+      }, 1000);
+
+      devSubmitButton.disabled = false;
+    }, 10000);
+  }
+});
+
+devFormModalButton.addEventListener("click", () => {
+  devFormModal.style.opacity = "0";
+  setTimeout(() => {
+    devFormModal.close();
+    devFormModal.style.opacity = "unset";
   }, 1000);
 });
